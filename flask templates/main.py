6a45c8aa4427eedaa27f6b04bf2ac0ee25c41832
Murod Turaev (24.01.2021) <tml.murod@mail.ru>
2021-03-03 11:51:44 +0500

    help

--- flask templates/main.py
+++ flask templates/main.py
@@ -1,10 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+html = "<h1>HTML</h1>"
+
 
 @app.route('/')
 def home():
-    return 'Home Page'
+    return html
+
+
+@app.route('/help')
+def help():
+    return html
 
 
 app.run(debug=True)
