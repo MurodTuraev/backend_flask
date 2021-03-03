08eafc56c223336c040679617757c1bcea214bda
Murod Turaev (24.01.2021) <tml.murod@mail.ru>
2021-03-03 13:56:20 +0500

    temp

--- flask templates/main.py
+++ flask templates/main.py
@@ -1,38 +1,125 @@
 from flask import Flask
 app = Flask(__name__)
 
-html = "<h1>HTML</h1>"
-
 
 @app.route('/')
 def home():
+    content = 'Home'
+    title = 'HOME'
     link = '''
     <a href='/aboutme'>About ME</a>
     <a href='/portfolio'>Portfolio</a>
     <a href='/contact'>Contact</a>
     <a href='/help'>Help</a>
     '''
-    return link
+    html_temp = f'''
+    <html>
+    <head>
+        <title> {title} </title>
+    </head>
+    <body>
+        <h1>{content}</h1>
+        {link}
+    </body>
+    </html>
+    '''
+    return html_temp
 
 
 @app.route('/aboutme')
 def aboutme():
-    return 'About ME'
+    content = 'Aboutme'
+    title = 'ABOUTME'
+    link = '''
+    <a href='/aboutme'>About ME</a>
+    <a href='/portfolio'>Portfolio</a>
+    <a href='/contact'>Contact</a>
+    <a href='/help'>Help</a>
+    '''
+    html_temp = f'''
+    <html>
+    <head>
+        <title> {title} </title>
+    </head>
+    <body>
+        <h1>{content}</h1>
+        {link}
+    </body>
+    </html>
+    '''
+    return html_temp
 
 
 @app.route('/portfolio')
 def portfolio():
-    return 'portfolio'
+    content = 'Portfolio'
+    title = 'PORTFOLIO'
+    link = '''
+    <a href='/aboutme'>About ME</a>
+    <a href='/portfolio'>Portfolio</a>
+    <a href='/contact'>Contact</a>
+    <a href='/help'>Help</a>
+    '''
+    html_temp = f'''
+    <html>
+    <head>
+        <title> {title} </title>
+    </head>
+    <body>
+        <h1>{content}</h1>
+        {link}
+    </body>
+    </html>
+    '''
+    return html_temp
 
 
 @app.route('/contact')
 def contact():
-    return 'Contact'
+    content = 'Contact'
+    title = 'CONTACT'
+    link = '''
+    <a href='/aboutme'>About ME</a>
+    <a href='/portfolio'>Portfolio</a>
+    <a href='/contact'>Contact</a>
+    <a href='/help'>Help</a>
+    '''
+    html_temp = f'''
+    <html>
+    <head>
+        <title> {title} </title>
+    </head>
+    <body>
+        <h1>{content}</h1>
+        {link}
+    </body>
+    </html>
+    '''
+    return html_temp
 
 
 @app.route('/help')
 def help():
-    return html
+    content = 'Help'
+    title = 'HELP'
+    link = '''
+    <a href='/aboutme'>About ME</a>
+    <a href='/portfolio'>Portfolio</a>
+    <a href='/contact'>Contact</a>
+    <a href='/help'>Help</a>
+    '''
+    html_temp = f'''
+    <html>
+    <head>
+        <title> {title} </title>
+    </head>
+    <body>
+        <h1>{content}</h1>
+        {link}
+    </body>
+    </html>
+    '''
+    return html_temp
 
 
 app.run(debug=True)
